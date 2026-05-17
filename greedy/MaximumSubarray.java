f5b673e2ca484aaaf367d6660c653613fe9c4a17
Krishna K N <krishnakn140@gmail.com>
2026-05-17 16:09:17 +0530

    added leetcode greedy maximum subarray

---
+++ greedy/MaximumSubarray.java
@@ -0,0 +1,48 @@
+// Maximum Subarray
+
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        
+        int curSum = 0;
+        int maxSum = nums[0];
+
+        for(int i=0; i<nums.length; i++) {
+            if(curSum < 0) {
+                curSum = 0;
+            }
+            curSum = curSum + nums[i];
+            maxSum = Math.max(curSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        MaximumSubarray obj = new MaximumSubarray();
+
+        int result = obj.maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
+
