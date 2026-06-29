4102d482b1471c60d9fbbe7cd79700b1911d8d8b
Krishna K N <krishnakn140@gmail.com>
2026-06-29 14:56:56 +0530

    added leetcode design lru cache and bit manipulation single number

---
+++ design/LRUCache.java
@@ -0,0 +1,150 @@
+// LRU Cache
+
+// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
+
+// Example 1:
+// Input
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+// Output
+// [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+// Explanation
+// LRUCache lRUCache = new LRUCache(2);
+// lRUCache.put(1, 1); // cache is {1=1}
+// lRUCache.put(2, 2); // cache is {1=1, 2=2}
+// lRUCache.get(1);    // return 1
+// lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+// lRUCache.get(2);    // returns -1 (not found)
+// lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+// lRUCache.get(1);    // return -1 (not found)
+// lRUCache.get(3);    // return 3
+// lRUCache.get(4);    // return 4
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class LRUCache {
+
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final HashMap<Integer, Node> map;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        remove(node);
+        insertAtHead(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            insertAtHead(node);
+        } else {
+            if(map.size() == capacity) {
+                map.remove(tail.prev.key);
+                remove(tail.prev);
+            }
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            insertAtHead(newNode);
+        }
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertAtHead(Node node) {
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] operations = {
+                "LRUCache", "put", "put", "get", "put",
+                "get", "put", "get", "get", "get"
+        };
+
+        int[][] values = {
+                {2},
+                {1, 1},
+                {2, 2},
+                {1},
+                {3, 3},
+                {2},
+                {4, 4},
+                {1},
+                {3},
+                {4}
+        };
+
+        List<Object> output = new ArrayList<>();
+
+        LRUCache cache = null;
+
+        for (int i = 0; i < operations.length; i++) {
+            String op = operations[i];
+
+            switch (op) {
+                case "LRUCache":
+                    cache = new LRUCache(values[i][0]);
+                    output.add(null);
+                    break;
+
+                case "put":
+                    cache.put(values[i][0], values[i][1]);
+                    output.add(null);
+                    break;
+
+                case "get":
+                    output.add(cache.get(values[i][0]));
+                    break;
+            }
+        }
+
+        System.out.println(output);
+    }
+}
+
