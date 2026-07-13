9b5d43a4c47fcd1fe1aab25385a16e404592e4d9
Krishna K N <krishnakn140@gmail.com>
2026-07-13 23:52:00 +0530

    added leetcode arrays first missing positive

---
+++ arrays/FirstMissingPositive.java
@@ -0,0 +1,85 @@
+// First Missing Positive
+
+// Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+// Example 1:
+// Input: nums = [1,2,0]
+// Output: 3
+// Explanation: The numbers in the range [1,2] are all in the array.
+
+// Example 2:
+// Input: nums = [3,4,-1,1]
+// Output: 2
+// Explanation: 1 is in the array but 2 is missing.
+
+// Example 3:
+// Input: nums = [7,8,9,11,12]
+// Output: 1
+// Explanation: The smallest positive integer 1 is missing.
+
+import java.util.Arrays;
+
+class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        
+        int n = nums.length;
+        int contains = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 1) {
+                contains++;
+                break;
+            }
+        }
+
+        if(contains == 0) {
+            return 1;
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
+            }
+        }
+
+        for(int i=0; i<nums.length; i++) {
+
+            int a = Math.abs(nums[i]);
+
+            if(a==n) {
+                nums[0] = -Math.abs(nums[0]);
+            } else {
+                nums[a] = -Math.abs(nums[a]);
+            }
+        }
+
+        for(int i=1; i<n; i++) {
+            if(nums[i] > 0) {
+                return i;
+            }
+        }
+
+        if(nums[0] > 0) {
+            return n;
+        }
+
+        return n+1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 4, -1, 1};
+
+        System.out.println("Input : " + Arrays.toString(nums));
+
+        FirstMissingPositive obj = new FirstMissingPositive();
+        int ans = obj.firstMissingPositive(nums);
+
+        System.out.println("Output: " + ans);
+    }
+
+}
+
+
