3a4001263f2893366380a5e88ed0bbbe096a20eb
Krishna K N <krishnakn140@gmail.com>
2026-08-06 23:11:04 +0530

    added leetcode sorting and searching search a 2d matrix

---
+++ sorting-and-searching/SearchA2DMatrix.java
@@ -0,0 +1,68 @@
+// Search a 2D Matrix
+
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+// Example 2:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+class SearchA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m*n-1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if(midValue == target) {
+                return true;
+            } else if(midValue < target) {
+                left = mid +1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target = 3;
+
+        SearchA2DMatrix obj = new SearchA2DMatrix();
+        boolean result = obj.searchMatrix(matrix, target);
+
+        System.out.println("Matrix:");
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nTarget: " + target);
+        System.out.println("Output: " + result);
+    }
+}
